795f68c4c4a2e21ed634dee214d6092ae245ac5c
Gleb Klimov <gklimov@gsmorg.com>
2025-10-30 01:11:05 +0100

    upgrade Kotlin to 2.2.20

--- kgraphql/build.gradle.kts
+++ kgraphql/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     base
 //    alias(libs.plugins.kotlin.jvm)
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "2.2.20"
     `maven-publish`
 }
 
